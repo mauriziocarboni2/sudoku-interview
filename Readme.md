a84917fd2f7c30e6216b88f487816dbfdcbbbdb6
Maurizio Carboni <maurizio.carboni@revelsystems.com>
2022-08-02 12:07:58 +0300

    Problem definition and testing tools

---
+++ Readme.md
@@ -0,0 +1,34 @@
+# Sudoku interview test
+
+## Introduction
+
+Sudoku is a Japanese game, the purpose of the game is simple, following 3 simple rules,
+you start from a partially filled board and you need to fill it fully.
+
+Only one solution exists for each partially filled board.
+
+The board is composed by `9x9` cells, the board is divided in 9 `3x3` boxes.
+
+![img.png](img.png)
+
+The rules are the following:
+
+1. Each number from `1 to 9` must occur in every row
+2. Each number from `1 to 9` must occur in every column
+3. Each number from `1 to 9` must occur in every `3x3` box
+
+The only valid solution for the sudoku combination above is:
+
+![img_1.png](img_1.png)
+
+## Test
+
+We really like Sudoku and we wanted to write a script that solves it for us, we started writing it, but we got stuck and
+we don't know how to complete.
+
+In this test you will try to complete the missing parts of the script so it will produce a valid output.
+
+## Input definition
+
+The function `solveSudoku` receives as input a matrix `9x9` the matrix is composed by one-character strings,
+valid values for these strings are: `123456789.`, the value '.' means that the box is not filled.
---
+++ src/solution.ts
@@ -0,0 +1,49 @@
+type CellValue = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '.'
+interface CacheMap {
+    row: Map<CellValue, boolean>[];
+    col: Map<CellValue, boolean>[];
+    box: Map<CellValue, boolean>[];
+}
+
+// We have nine boxes in a sudoku board, this function let us know for a pair (y,x) which box is
+const getBoxId = (y: number, x: number): number =>
+    Math.floor(y/3)* 3 + Math.floor(x/3);
+
+// Return if the value _value_ can be inserted in position ( y, x ) without conflicting with the values already present in the board
+const isValidValue = (y: number, x: number, box: number, value: CellValue, cacheMap: CacheMap): boolean =>
+    // ToDo: complete this function
+    false;
+
+// This function checks if the combination is valid
+const isValidSudokuCombination = (board: CellValue[][], cacheMap: CacheMap): boolean => {
+    for (let y=0; y<9; y++) {
+        for (let x=0; x<9; x++) {
+            if (board[y][x] === ".") {
+                // ToDo: This cell is empty, add the code to fill it
+            }
+        }
+    }
+    // if we didn't return before, it means no invalid value has been put in the board, and so the combination is valid
+    // on top of that the sudoku is now solved because in order to check we filled it
+    return true
+}
+/**
+ Do not return anything, modify board in-place instead.
+ */
+export function solveSudoku(board: CellValue[][]): void {
+    const cacheMap: CacheMap = {
+        row: Array.from({ length: 9 }).map(_ => new Map<CellValue, boolean>()),
+        col: Array.from({ length: 9 }).map(_ => new Map<CellValue, boolean>()),
+        box: Array.from({ length: 9 }).map(_ => new Map<CellValue, boolean>()),
+    }
+    // Construct the cache, the purpose of the cache is to avoid scanning the board every time
+    for (let y=0; y<9; y++)
+        for (let x=0; x<9; x++)
+            if (board[y][x] !== ".") {
+                cacheMap.row[y].set(board[y][x], true);
+                cacheMap.col[x].set(board[y][x], true);
+                cacheMap.box[getBoxId(y, x)].set(board[y][x], true);
+            }
+    // unless the board received as input is invalid, this function should always return true
+    isValidSudokuCombination(board, cacheMap);
+};
